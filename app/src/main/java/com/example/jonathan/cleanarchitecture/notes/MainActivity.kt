f2061e6ce0a51f7c8dde3bd222e09355a190f11f
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2021-10-02 15:37:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jonathan/cleanarchitecture/notes/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.jonathan.cleanarchitecture.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jonathan.cleanarchitecture.notes.ui.theme.NotesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NotesAppTheme {
+        Greeting("Android")
+    }
+}
